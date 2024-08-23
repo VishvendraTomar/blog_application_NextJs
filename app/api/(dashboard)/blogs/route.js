5a75742f4b69eb06b25b60ae1614159891cf7abb
vishvendra <vishvendra@geekyants.com>
2024-08-23 12:49:17 +0530

    added post , delete , single blog routes

--- app/api/(dashboard)/blogs/route.js
+++ app/api/(dashboard)/blogs/route.js
@@ -41,3 +41,43 @@ export const GET = async (request) => {
     return new NextResponse("Error in fetching blogs: " + err.message, { status: 500 });
   }
 };
+
+
+export const POST = async (request) => {
+  try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+    const categoryId = searchParams.get("categoryId");
+    const { title, description } = await request.json();
+
+    if (!userId || !Types.ObjectId.isValid(userId)) {
+      return new NextResponse("Invalid user ID", { status: 400 });
+    }
+    if (!categoryId || !Types.ObjectId.isValid(categoryId)) {
+      return new NextResponse("Invalid category ID", { status: 400 });
+    }
+
+    await connect();
+    const user = await User.findById(userId);
+    if (!user) {
+      return new NextResponse("User not found", { status: 404 });
+    }
+
+    const category = await Category.findById(categoryId);
+    if (!category) {
+      return new NextResponse("Category not found", { status: 404 });
+    }   
+
+    const newBlog = new Blog({
+      title,
+      description,
+      user: userId,
+      category: categoryId
+    });
+    await newBlog.save();
+    return new NextResponse(JSON.stringify(newBlog), { status: 200 });
+  } catch (error) {
+    console.error("Error in creating blog: ", error);
+    return new NextResponse("Error in creating blog: " + error.message, { status: 500 });
+  }
+}
