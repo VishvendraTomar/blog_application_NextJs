a7215b844f0d70392e124c3437d13f4675380a9a
vishvendra <vishvendra@geekyants.com>
2024-08-28 11:21:57 +0530

    removed middleware

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,11 @@
+import React from "react"
+import GetUserInformaiont from "./button"
 
 const userInformation = () => {
   return (
     <div>
       <h1>user Detail page</h1>
+      <GetUserInformaiont/>
     </div>
   )
 }
